175dc92ad48f9d9be5aa042b002994913d4ade5c
Mikhail Budkov <mbudkov@ozon.ru>
2022-05-22 22:51:09 +0200

    Hello world chapter

---
+++ hello_world/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const (
+	englishHelloPrefix = "Hello,"
+	spanishHelloPrefix = "Holla,"
+	Spanish            = "Spanish"
+)
+
+func getPrefixByLanguage(lang string) string {
+	switch lang {
+	case Spanish:
+		return spanishHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
+}
+
+// Hello function generate greeting message
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+	helloPrefix := getPrefixByLanguage(lang)
+	return fmt.Sprintf("%v %v", helloPrefix, name)
+}
+
+func main() {
+	fmt.Println(Hello("Mikhail", ""))
+}
---
+++ hello_world/hello_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Mikhail", "")
+		want := "Hello, Mikhail"
+		assertMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Mikhail", "Spanish")
+		want := "Holla, Mikhail"
+		assertMessage(t, got, want)
+	})
+}
